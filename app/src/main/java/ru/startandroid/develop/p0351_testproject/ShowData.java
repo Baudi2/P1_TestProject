3af2157a5adf466531a317b16bf62d47cbda8f3d
Baudi2 <badicyusupov@gmail.com>
2020-08-22 11:52:16 +0300

    little changes in ShowData, deleted unnecessary lines of code

--- app/src/main/java/ru/startandroid/develop/p0351_testproject/ShowData.java
+++ app/src/main/java/ru/startandroid/develop/p0351_testproject/ShowData.java
@@ -2,6 +2,7 @@ package ru.startandroid.develop.p0351_testproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class ShowData extends AppCompatActivity {
     TextView tvText, tvText_2;
     ImageView imageView;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,28 +43,24 @@ public class ShowData extends AppCompatActivity {
             int company_Index = cursor.getColumnIndex(DBHelper.KEY_COMPANY);
             int url_Index = cursor.getColumnIndex(DBHelper.KEY_URL);
             int text_Index = cursor.getColumnIndex(DBHelper.KEY_TEXT);
-            String url = cursor.getString(avatar_Index);
 
             do {
                 Log.d(MainActivity.LOG_TAG,  cursor.getString(id_Index) + cursor.getString(email_Index) + cursor.getString(first_name_Index)+
                         cursor.getString(last_name_Index) + cursor.getString(avatar_Index) + cursor.getString(company_Index) + cursor.getString(url_Index)
                 + cursor.getString(text_Index));
                 // собираем строку для отображение данных из базы
-                String sent = "id: " + cursor.getString(id_Index) + "\n" + "Email: " + cursor.getString(email_Index) +
+                tvText.setText("id: " + cursor.getString(id_Index) + "\n" + "Email: " + cursor.getString(email_Index) +
                         "\n" +  "First name: " + cursor.getString(first_name_Index) + "\n" +  "Last name: " +
-                        cursor.getString(last_name_Index) + "\n" +  "Avatar: " + "\n";
-                tvText.setText(sent);
-                Glide.with(this).load(url).into(imageView);
-                String sent2 = "Company: " + cursor.getString(company_Index) + "\n" + "Url: " + cursor.getString(url_Index) + "\n" +
-                      "Text: " + cursor.getString(text_Index);
-                tvText_2.setText(sent2);
+                        cursor.getString(last_name_Index) + "\n" +  "Avatar: " + "\n");
+                Glide.with(this).load(cursor.getString(avatar_Index)).into(imageView);
+                tvText_2.setText("Company: " + cursor.getString(company_Index) + "\n" + "Url: " + cursor.getString(url_Index) + "\n" +
+                        "Text: " + cursor.getString(text_Index));
             } while (cursor.moveToNext());
         }// if
         // если в таблице нет данных
         else {
             Log.d(MainActivity.LOG_TAG, "0 rows");
-            String diff = "Database is empty";
-            tvText.setText(diff);
+            tvText.setText("Database is empty");
         }// else
         cursor.close();
 
